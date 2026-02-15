a5b80ff86bb75d781a4fb52f41549d46c77d2abe
Meir-Holzer <meirh1900@gmail.com>
2026-02-15 13:02:38 +0200

    מילון בסיסי עם תובנה של תמונות שצולמו ברדיוס קרוב

--- image_intel/src/analyzer.py
+++ image_intel/src/analyzer.py
@@ -49,8 +49,8 @@ fake_data = [
     {
         "filename": "test6.jpg",
         "datetime": "2025-01-15 07:45:22",
-        "latitude": 29.5577,
-        "longitude": 34.9519,
+        "latitude": 32.794000,
+        "longitude": 34.989600,
         "camera_make": "Canon",
         "camera_model": "EOS R6",
         "has_gps": True
@@ -58,8 +58,8 @@ fake_data = [
     {
         "filename": "test7.jpg",
         "datetime": "2025-01-16 22:18:03",
-        "latitude": 32.3215,
-        "longitude": 34.8532,
+        "latitude": 32.801500,
+        "longitude": 34.990500,
         "camera_make": "Nikon",
         "camera_model": "D750",
         "has_gps": True
@@ -67,8 +67,8 @@ fake_data = [
     {
         "filename": "test8.jpg",
         "datetime": "2025-01-17 11:02:37",
-        "latitude": 31.0461,
-        "longitude": 34.8516,
+        "latitude": 32.085300,
+        "longitude": 34.781800,
         "camera_make": "Sony",
         "camera_model": "Alpha A7 IV",
         "has_gps": True
@@ -76,8 +76,8 @@ fake_data = [
     {
         "filename": "test9.jpg",
         "datetime": "2025-01-18 16:29:54",
-        "latitude": 33.0156,
-        "longitude": 35.2700,
+        "latitude": 32.087000,
+        "longitude":  34.783000,
         "camera_make": "Samsung",
         "camera_model": "Galaxy S22",
         "has_gps": True
@@ -93,6 +93,13 @@ fake_data = [
     }
 
 ]
+"""
+analyzer.py - ניתוח דפוסים וזיהוי קשרים
+צוות 2, זוג B
+
+ראו docs/api_contract.md לפורמט הקלט והפלט.
+"""
+
 
 def sorted_dict(images_data):
     sorted_images = sorted(
@@ -119,21 +126,40 @@ def detect_camera_switches(sorted_images):
             })
     return switches
 
-"""
-analyzer.py - ניתוח דפוסים וזיהוי קשרים
-צוות 2, זוג B
 
-ראו docs/api_contract.md לפורמט הקלט והפלט.
-"""
+
+def distance(image_data):
+    distance_list = []
+    for i in range(len(image_data)):
+        place = [image_data[i]["filename"]]
+        if image_data[i].get("latitude") and image_data[i].get("longitude"):
+            for j in range(len(image_data)):
+                if i == j:
+                    continue
+                if image_data[j].get("latitude") and image_data[j].get("longitude"):
+                    point1 = image_data[i]["latitude"],image_data[i]["longitude"]
+                    point2 = image_data[j]["latitude"],image_data[j]["longitude"]
+                    if geodesic(point1, point2).kilometers < 1:
+                        place.append(image_data[j]["filename"])
+        place.sort()
+        if len(place) > 1 and place not in distance_list:
+            distance_list.append(place)
+    return distance_list
+
+def radius_1(distance_list):
+    d = {'תמונות שצולמו ברדיוס של 1 ק"מ':[]}
+    for distance in distance_list:
+        d['תמונות שצולמו ברדיוס של 1 ק"מ'].append(distance)
+    return d
 
 
 def analyze(images_data):
     """
     מנתח את הנתונים ומוצא דפוסים.
-    
+
     Args:
         images_data: רשימת מילונים מ-extract_all
-    
+
     Returns:
         dict עם: total_images, images_with_gps, unique_cameras,
               date_range, insights
@@ -143,11 +169,10 @@ def analyze(images_data):
         "images_with_gps": 0,
         "unique_cameras": 0,
         "date_range": 0,
+        "insights": []
     }
     count_gps = 0
     cameras = set()
-
-
 
     for image in images_data:
         if image["has_gps"]:
@@ -157,40 +182,8 @@ def analyze(images_data):
     dict_result["total_images"] = len(images_data)
     dict_result["images_with_gps"] = count_gps
     dict_result["unique_cameras"] = cameras
-    dict_result["date_range"] = [images_data[0]["datetime"],images_data[-1]["datetime"]]
-
+    dict_result["date_range"] = [images_data[0]["datetime"], images_data[-1]["datetime"]]
+    dict_result["insights"] = radius_1(distance(images_data))
     return dict_result
 
-# def cities(images_data):
-#     address = []
-#     geolocator = Nominatim(user_agent="geo_example")
-#     for image in images_data:
-#         latitude = image.get("latitude")
-#         longitude = image.get("longitude")
-#         if latitude and longitude:
-#             location =  geolocator.reverse((latitude, longitude), language='he')
-#             adrr = location.row['address']
-#             address.append(adrr)
-#     return address
-# print(cities(sorted_dict(fake_data)))
-
-from geopy.geocoders import Nominatim
-
-def distance(image_data):
-    distance_list = []
-    for i in range(len(image_data)):
-        if image_data[i].get("latitude") and image_data[i].get("longitude"):
-            for j in range(len(image_data)):
-                if i == j:
-                    continue
-                place = [image_data[i]["filename"]]
-                if image_data[j].get("latitude") and image_data[j].get("longitude"):
-                    point1 = image_data[i]["latitude"],image_data[i]["longitude"]
-                    point2 = image_data[j]["latitude"],image_data[j]["longitude"]
-                    if geodesic(point1, point2).kilometers < 1:
-                        distance_list.append(image_data[j]["filename"])
-        if len(place) > 1 and place not in distance_list:
-            distance_list.append(place)
-    return distance_list
-
-print(distance(fake_data))
+print(analyze(fake_data))
